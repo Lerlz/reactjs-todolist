ec8bee9c658bcfe43af3d253d10982008fa9fb10
Lerlz <lerlynn@channelmobile.co.za>
2025-01-17 12:44:09 +0200

    Update TodoInput.jsx

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,16 +1,31 @@
-import { useState } from "react"
+import { useState } from "react";
 
 export default function TodoInput(props) {
-    const { handleAddTodos, todoValue, setTodoValue } = props
+    const { handleAddTodos, todoValue, setTodoValue } = props;
+
+    const handleKeyDown = (e) => {
+        if (e.key === "Enter") {
+            handleAddTodos(todoValue);
+            setTodoValue("");
+        }
+    };
+
     return (
         <header>
-            <input value={todoValue} onChange={(e) => {
-                setTodoValue(e.target.value)
-            }} placeholder="Enter todo..." />
-            <button onClick={() => {
-                handleAddTodos(todoValue)
-                setTodoValue('')
-            }}>Add</button>
+            <input
+                value={todoValue}
+                onChange={(e) => setTodoValue(e.target.value)}
+                onKeyDown={handleKeyDown}
+                placeholder="Enter todo..."
+            />
+            <button
+                onClick={() => {
+                    handleAddTodos(todoValue);
+                    setTodoValue("");
+                }}
+            >
+                Add
+            </button>
         </header>
-    )
+    );
 }
