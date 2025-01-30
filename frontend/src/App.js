1bf983fcdd45508282968bfd40aa5674a71d153f
Lerlynn <lerlynn@channelmobile.co.za>
2025-01-30 10:02:49 +0200

    Fixed missing index.js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>My Todo List</h1>
+      {/* Add your Todo List components here */}
         <a
           className="App-link"
           href="https://reactjs.org"
