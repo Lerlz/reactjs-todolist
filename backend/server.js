0c4a1cf535625a3a0d40835e17d30b74efed6039
Lerlynn <lerlynn@channelmobile.co.za>
2025-02-07 12:56:48 +0200

    Add .gitignore
    Add Dockerfile
    
    Add database docker-compose
    
    Add backend Dockerfile
    
    Add backend server
    
    Add Todo model
    
    Add Todo routes
    
    Add backend env

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-require("dotenv").config();
+const dotnev = require("dotenv").config();
+const todoRoutes = require("./routes/todoRoutes");
 
+dotnev.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
